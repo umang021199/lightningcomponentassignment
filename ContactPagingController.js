97b08b803d13db8c2b75ffe9eda3d3cbfcc10da7
umang021199 <63451339+umang021199@users.noreply.github.com>
2021-03-17 14:30:00 +0530

    Create ContactPagingController.js

---
+++ ContactPagingController.js
@@ -0,0 +1,26 @@
+({
+    doInit: function(component, event, helper) {
+        var pageNumber = component.get("v.PageNumber");  
+        var pageSize = component.get("v.Selectedvalue"); 
+        helper.getContactList(component, pageNumber, pageSize);
+    },
+    
+	handleNext: function(component, event, helper) {
+        var pageNumber = component.get("v.PageNumber");  
+        var pageSize = component.get("v.Selectedvalue");
+        pageNumber++;
+        helper.getContactList(component, pageNumber, pageSize);
+    },
+    handlePrev: function(component, event, helper) {
+        var pageNumber = component.get("v.PageNumber");  
+        var pageSize = component.get("v.Selectedvalue");
+        pageNumber--;
+        helper.getContactList(component, pageNumber, pageSize);
+    },
+     
+    onSelectChange: function(component, event, helper) {
+        var page = 1
+        var pageSize = component.get("v.Selectedvalue");
+        helper.getContactList(component, page, pageSize);
+    },
+})
